54382203d55e7c0beee90a1acb0c942036cd625d
smmmfrd <smmmfrd@gmail.com>
2024-08-21 21:12:38 -0700

    Logic: Movement logic given to character body 2D.

--- player_controller.gd
+++ player_controller.gd
@@ -1,22 +1,14 @@
 extends CharacterBody2D
 
-const JUMP_VELOCITY = -400.0
+@export var jump_velocity = 500.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var ground_level : float
-
-func _ready():
-	ground_level = global_position.y
 
 func _physics_process(delta):
-	if global_position.y < ground_level:
-		velocity.y += gravity * delta
-	else:
-		velocity.y = 0
-		global_position.y = ground_level
+	velocity.y += gravity * delta
 	
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y = -jump_velocity
 	
 	move_and_slide()
 
